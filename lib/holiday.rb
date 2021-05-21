608a10c72323629d27f6c6a41d3d19f632414060
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-21 17:46:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, items|
       season = season.to_s.capitalize
-      holiday = holiday.to_s.split("_").capitalize
+      holiday = holiday.to_s.split("_").capitalize.join(" ")
       items = items.join(", ")
       puts "#{season}:"
       puts "  #{holiday}: #{items}"
